ffa5e7d7826fa9f6825280b64cc82b2be40d0f4c
Nilesh Tanchak <nileshtanchak.coderkube@gmail.com>
2026-04-29 09:57:24 +0530

    updatem README.md file

--- lib/core/network/api_endpoints.dart
+++ lib/core/network/api_endpoints.dart
@@ -1,7 +1,7 @@
 /// A central repository for all API endpoint strings used in the application.
 class ApiEndpoints {
   static const String baseUrl = 'https://api.themoviedb.org/3';
-  static const String apiKey = 'YOUR_API_KEY';
+  static const String apiKey = 'YOUR_API_KEY_HERE';
   
   /// Endpoint to discover movies.
   static String discoverMovies(int page) => '/discover/movie?api_key=$apiKey&page=$page';
